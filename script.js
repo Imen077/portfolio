a1ddcd9a360677c00af5c0669b329a2641ea9f07
Imen007 <faeyzayudistira07@gmail.com>
2024-09-03 13:21:18 +0700

    firts commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu");
+const burgers = document.getElementsByClassName ("burger");
+const content = document.querySelector(".menu-content");
+
+menuButton.addEventListener("click", () => {
+    burgers(0).classList.toggle("rotate-1");
+    burgers(1).classList.toggle(hide);
+    burgers(2).classList.toggle("rotate-2");
+});
